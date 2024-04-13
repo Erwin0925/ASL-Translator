c782ac52e2078a5ff813175295c21e61465e55d6
Erwin <tp064564@gmail.com>
2024-04-13 23:08:44 +0800

    Update translate.py

--- ASL_Translation_System/translate.py
+++ ASL_Translation_System/translate.py
@@ -7,7 +7,6 @@ import mediapipe as mp
 from sign_detection import mediapipe_detection, draw_styled_landmarks, extract_keypoints
 import tensorflow as tf
 from collections import Counter
-from webcam_manager import WebcamManager
 
 # Load MediaPipe holistic model and drawing utilities
 mp_holistic = mp.solutions.holistic
@@ -20,22 +19,24 @@ actions = np.array(["Family", "Friends", "Work", "School", "Home", "Car", "Happy
 LSTM_model = tf.keras.models.load_model("LSTM_Model.h5")
 
 class TranslatePage(tk.Frame):
-    def __init__(self, parent, controller,webcam_manager):
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='white')
         self.controller = controller
-        self.webcam_manager = webcam_manager
+
         # Setup top bar, navigation buttons, etc.
         self.setup_ui_components()
 
         self.sequence = []
         self.predictions = []
-        self.res = [0] * len(actions)
+        #self.res = [0] * len(actions)
         self.threshold = 0.5
         self.predicting = False
         self.start_translating = False
         self.wait_frames = 0
         self.frame_count = 0
-        
+        self.selected_word = None
+        self.display_word = ""
+        self.result_display = ""
 
         # Initialize OpenCV video capture object
         self.initialize_video_capture()
@@ -55,8 +56,7 @@ class TranslatePage(tk.Frame):
         translate_button = tk.Button(nav_bar, text="Translate", bg='#CAF0F8', fg='#03045E', font=("Roboto", 14), height=2, borderwidth=0)
         translate_button.pack(side='left', expand=True, fill='x')
 
-        test_button = tk.Button(nav_bar, text="Test", bg='#5D8FB3', fg='#03045E', font=("Roboto", 14), height=2, borderwidth=0, 
-                                command=lambda: self.controller.show_frame("TestPage"))
+        test_button = tk.Button(nav_bar, text="Test", bg='#5D8FB3', fg='#03045E', font=("Roboto", 14), height=2, borderwidth=0)
         test_button.pack(side='left', expand=True, fill='x')
 
         self.video_area = tk.Label(self, bg='white')  # Create a Label widget
@@ -73,12 +73,11 @@ class TranslatePage(tk.Frame):
         self.translated_word_label.config(text=translated_word)
     
     def initialize_video_capture(self):
-        try:
-            self.webcam_manager.open_webcam()
-            self.start_video_stream()
-        except Exception as e:
-            messagebox.showerror("Webcam Error", str(e))
-
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            messagebox.showerror("Webcam Error", "Could not open webcam.")
+            return
+        self.start_video_stream()
 
     def start_video_stream(self):
         self.holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -89,60 +88,59 @@ class TranslatePage(tk.Frame):
         self.predictions.clear()
 
     def update_video_feed(self):
-        try:
-            frame = self.webcam_manager.get_frame()
-            if frame is not None:
-                # Process the frame (mediapipe detection, drawing landmarks, etc.)
-                image, results = mediapipe_detection(frame, self.holistic)
-                draw_styled_landmarks(image, results)
+        ret, frame = self.cap.read()
+        if ret:
+            # Process the frame (mediapipe detection, drawing landmarks, etc.)
+            image, results = mediapipe_detection(frame, self.holistic)
+            draw_styled_landmarks(image, results)
 
-                if self.start_translating:
-                    self.frame_count = 0
-                    self.wait_frames = 30  # Adjust this value as needed
+            if self.start_translating:
+                self.frame_count = 0
+                self.wait_frames = 30  # Adjust this value as needed
+                self.start_translating = False
+                self.predicting = True
+
+            # Display the "Start Translating" text for the duration of self.wait_frames
+            if self.wait_frames > 0:
+                cv2.putText(image, "Start Translation", (190, 100),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (94, 4, 3), 2, cv2.LINE_AA)
+                self.wait_frames -= 1
+
+            if self.predicting and self.wait_frames <= 0:
+                if self.frame_count < 30:
+                    keypoints = extract_keypoints(results)
+                    self.sequence.append(keypoints)
+                    self.sequence = self.sequence[-30:]
+
+                    if len(self.sequence) == 30:
+                        res = LSTM_model.predict(np.expand_dims(self.sequence, axis=0), verbose=0)[0]
+                        # res = LSTM_model.predict(np.expand_dims(self.sequence, axis=0))[0]
+                        self.predictions.append(np.argmax(res))
+                        
+                    self.frame_count += 1
+                else:
+                    # Determine the most frequent action after 30 frames
+                    if self.predictions:
+                        most_common = Counter(self.predictions).most_common(1)[0][0]
+                        action = actions[most_common]
+                        self.display_translated_word(action)
+
+                    # Reset prediction mode
+                    self.predicting = False
                     self.start_translating = False
-                    self.predicting = True
-
-                # Display the "Start Translating" text for the duration of self.wait_frames
-                if self.wait_frames > 0:
-                    cv2.putText(image, "Start Translation", (190, 100),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (94, 4, 3), 2, cv2.LINE_AA)
-                    self.wait_frames -= 1
-
-                if self.predicting and self.wait_frames <= 0:
-                    if self.frame_count < 30:
-                        keypoints = extract_keypoints(results)
-                        self.sequence.append(keypoints)
-                        self.sequence = self.sequence[-30:]
-
-                        if len(self.sequence) == 30:
-                            res = LSTM_model.predict(np.expand_dims(self.sequence, axis=0), verbose=0)[0]
-                            self.predictions.append(np.argmax(res))
-                            
-                        self.frame_count += 1
-                    else:
-                        # Determine the most frequent action after 30 frames
-                        if self.predictions:
-                            most_common = Counter(self.predictions).most_common(1)[0][0]
-                            action = actions[most_common]
-                            self.display_translated_word(action)
-
-                        # Reset prediction mode
-                        self.predicting = False
-                        self.start_translating = False
-                        self.frame_count = 0
-                
-                cv_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                img = Image.fromarray(cv_img)
-                imgtk = ImageTk.PhotoImage(image=img)
-                self.video_area.imgtk = imgtk  # Keep reference to avoid garbage collection
-                self.video_area.configure(image=imgtk)
-
-        except Exception as e:
-            messagebox.showerror("Webcam Error", str(e))
+                    self.frame_count = 0
+            
+            cv_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv_img)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.video_area.imgtk = imgtk  # Keep reference to avoid garbage collection
+            self.video_area.configure(image=imgtk)
 
         self.after(10, self.update_video_feed)  # Continue updating the feed
 
     def destroy(self):
-        # Release the webcam using the webcam manager
-        self.webcam_manager.release_webcam()
+        # Release resources when closing the application
+        if self.cap.isOpened():
+            self.cap.release()
+        self.holistic.close()
         super().destroy()
